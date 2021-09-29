b09c24b16415b4d8af58977ca0c9f594f5a12db8
ScienceRocket <langenhovenh@yahoo.com>
2021-09-29 13:13:33 +0200

    init check in

---
+++ Dev07/iosbuild.bat
@@ -0,0 +1,2 @@
+cd C:\Users\ScienceJ\source\repos\Dev07\Dev07\
+
---
+++ Dev07/www/config.js
@@ -0,0 +1,6 @@
+
+//var defaultServerUrl = "http://gidevvmsrv03/dev/GiscoeOfflineBuilder/";
+//var defaultServerUrl = "https://GHDEVVMSRV04.giscoe.com/GiscoeOfflineBuilder/";
+var defaultServerUrl = "https://ghprodsrv03.giscoe.com/FaultReporter/";
+var guestUser = "Guest";
+var FilterOption = new Object();  
