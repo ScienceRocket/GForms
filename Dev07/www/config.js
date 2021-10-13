48629e5df8cd435b24394158e36029d2403cf11e
ScienceJ <langenhovenh@yahoo.com>
2021-10-13 14:24:54 +0200

    New

--- Dev07/www/config.js
+++ Dev07/www/config.js
@@ -1,6 +1,6 @@
 
 //var defaultServerUrl = "http://gidevvmsrv03/dev/GiscoeOfflineBuilder/";
 //var defaultServerUrl = "https://GHDEVVMSRV04.giscoe.com/GiscoeOfflineBuilder/";
-var defaultServerUrl = "https://ghprodsrv03.giscoe.com/WebApps/KumbaGForms/";
+var defaultServerUrl = "https://ghprodsrv03.giscoe.com/GiscoeInternalSystem/";
 var guestUser = "Guest";
 var FilterOption = new Object();    
