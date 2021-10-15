5d0ced05ccac68bd53a859faa2edf1e64fa19729
ScienceJ <langenhovenh@yahoo.com>
2021-10-15 06:54:31 +0200

    Kumba Version

--- Dev07/www/config.js
+++ Dev07/www/config.js
@@ -1,6 +1,6 @@
 
 //var defaultServerUrl = "http://gidevvmsrv03/dev/GiscoeOfflineBuilder/";
 //var defaultServerUrl = "https://GHDEVVMSRV04.giscoe.com/GiscoeOfflineBuilder/";
-var defaultServerUrl = "https://ghprodsrv03.giscoe.com/GiscoeInternalSystem/";
+var defaultServerUrl = "https://ghprodsrv03.giscoe.com/WebApps/KumbaGforms/";
 var guestUser = "Guest";
 var FilterOption = new Object();    
